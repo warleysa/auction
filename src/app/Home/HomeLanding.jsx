18babf0c43ff48b48e1542da5eb897551e968c4c
Meanings11 <mingful@smu.edu>
2019-04-23 00:04:35 -0500

    background img

---
+++ src/app/Home/HomeLanding.jsx
@@ -0,0 +1,38 @@
+/* eslint eqeqeq: "off" */
+import React from 'react';
+import { RealRepo } from '../../api/realRepo';
+import { AuctionList } from '../Auction/AuctionList';
+import { Car } from '../../models/car';
+import HomePage from './HomePage';
+
+
+export class HomeLanding extends React.Component {
+	realRepo = new RealRepo();
+
+    
+
+    state = {
+        auctions: []
+    };
+
+
+    render() {
+        return (
+            <>
+                        <HomePage style={{height: '400px'}}/>
+						<AuctionList auctions={this.state.auctions} userZipCode={this.props.userInfo.userZipCode}/>
+            </>
+        );
+    }
+
+		componentDidMount() {
+			this.realRepo.getAuctions()
+				.then(auctions => {
+					let cars = auctions.map(c => new Car(c.UserId, c.Make, c.Model, c.Year, "No API Data for Mileage", c.Zip, c.Description, c.Price, c.StartTime, c.EndTime, +c.AuctionId));
+					this.setState({auctions: cars});
+				});
+		}
+}
+
+
+export default HomeLanding;
---
+++ src/app/Home/HomePage.jsx
@@ -0,0 +1,8 @@
+import './HomePage.css';
+import React, {Component} from 'react';
+
+export const HomePage = (props) =>(
+    <div className="bg"></div>
+);
+
+export default HomePage;
