06bd2105baba56b815415d0c211bc38576492f5e
warleysam <warley.sam@gmail.com>
2019-05-01 18:04:06 -0500

    update

---
+++ build/precache-manifest.e2509217b6f14af1c061a6c16a4f6317.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "946973e70b176731dcdf",
+    "url": "/static/css/main.9a289c45.chunk.css"
+  },
+  {
+    "revision": "946973e70b176731dcdf",
+    "url": "/static/js/main.85c1c2ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "609b7c74007d29da9175",
+    "url": "/static/css/2.8a97253b.chunk.css"
+  },
+  {
+    "revision": "609b7c74007d29da9175",
+    "url": "/static/js/2.b31ddb39.chunk.js"
+  },
+  {
+    "revision": "4b12aac575ec3b4142e215dd3d3745ef",
+    "url": "/static/media/loadingSpinner.4b12aac5.gif"
+  },
+  {
+    "revision": "f07d66e00150e23a0d8b2e10b7b0f86b",
+    "url": "/static/media/man.f07d66e0.png"
+  },
+  {
+    "revision": "7ad289827b06021be89a458ef933f9f2",
+    "url": "/static/media/Luigi.7ad28982.png"
+  },
+  {
+    "revision": "4d2da6b50077f9657c063c4f48537484",
+    "url": "/static/media/logo.4d2da6b5.ico"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "68f3f73256b6868ca2986a37c7a0792c",
+    "url": "/index.html"
+  }
+];
